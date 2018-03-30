21d37f2542b162151e12947c7331ae6cb992cec6
Yves Hoppe <yves.hoppe@community.joomla.org>
2018-03-30 14:59:48 +0200

    Added task for starting selenium server with the correct WebDriver

--- src/Robo/Plugin/Commands/TestCommands.php
+++ src/Robo/Plugin/Commands/TestCommands.php
@@ -4,7 +4,6 @@ namespace JoomlaRobo\Robo\Plugin\Commands;
 
 class TestCommands extends \Robo\Tasks
 {
-    use \JoomlaRobo\Task\Scaffold\Tasks;
     use \JoomlaRobo\Task\Test\Tasks;
 
     /**
@@ -17,38 +16,6 @@ class TestCommands extends \Robo\Tasks
     public function Selenium($name)
     {
         $this->taskRunSelenium($name)
-            ->run();
-    }
-
-    /**
-     * Scaffold a module extensions
-     * 
-     * @command Scaffold:Module
-     * 
-     * @param string $name
-     * @param string $baseDir
-     */
-    public function Module($name, $baseDir = 'code')
-    {
-        $this->taskScaffoldModule($name)
-            ->setBaseDir($baseDir)
-            ->run();
-    }
-
-    /**
-     * Scaffold a plugin extensions
-     * 
-     * @command Scaffold:Plugin
-     * 
-     * @param string $name
-     * @param string $type
-     * @param string $baseDir
-     */
-    public function Plugin($name, $type, $baseDir = 'code')
-    {
-        $this->taskScaffoldPlugin($name)
-            ->setBaseDir($baseDir)
-            ->setType($type)
             ->run();
     }
 }
